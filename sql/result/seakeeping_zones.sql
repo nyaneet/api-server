c5a1cf648c648511ea76bb4e89a5d14172b65a5a
Zack <113541780+Klaimmoro@users.noreply.github.com>
2025-08-28 17:48:31 +0300

    Update seakeeping_zones.sql

--- sql/result/seakeeping_zones.sql
+++ sql/result/seakeeping_zones.sql
@@ -1,4 +1,8 @@
-CREATE TYPE IF NOT EXISTS zone_type AS ENUM ('Parametric', 'Main', 'Broaching', 'HighWaves');
+DROP TABLE IF EXISTS seakeeping_zones CASCADE;
+
+DROP TYPE IF EXISTS zone_type CASCADE;
+
+CREATE TYPE zone_type AS ENUM ('Parametric', 'Main', 'Broaching', 'HighWaves');
 
 CREATE TABLE IF NOT EXISTS seakeeping_zones (
     id SERIAL PRIMARY KEY,
