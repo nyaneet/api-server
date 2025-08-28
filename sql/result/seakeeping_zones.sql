43cbf115409b75301ff4393bbb7b92bb27d94f4a
Zack <113541780+Klaimmoro@users.noreply.github.com>
2025-08-28 18:10:01 +0300

    Update seakeeping_zones.sql

--- sql/result/seakeeping_zones.sql
+++ sql/result/seakeeping_zones.sql
@@ -6,9 +6,11 @@ CREATE TYPE zone_type AS ENUM ('Parametric', 'Main', 'Broching', 'HighWaves');
 
 CREATE TABLE IF NOT EXISTS seakeeping_zones (
     id SERIAL PRIMARY KEY,
+    project_id INT,
+    ship_id INT NOT NULL,
     angle FLOAT NOT NULL,
     speed FLOAT NOT NULL,
     zone_id zone_type NOT NULL,
-    
+    CONSTRAINT criterion_values_criterions_id_unique UNIQUE NULLS NOT DISTINCT (project_id, ship_id),
     CONSTRAINT unique_angle_speed_zone UNIQUE (angle, speed, zone_id)
 );
