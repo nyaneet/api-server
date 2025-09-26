32ee2b1ee785207fe584087b00e232daade0aa08
povergo <korolev.petr2003@gmail.com>
2025-09-26 15:42:53 +0300

    DB, waypoint | create port_id

--- sql/voyage/voyage_waypoint_create.sql
+++ sql/voyage/voyage_waypoint_create.sql
@@ -6,6 +6,7 @@ CREATE TABLE
         id INT GENERATED ALWAYS AS IDENTITY, -- ID of the waypoint
         project_id INT, -- ID of the project
         ship_id INT NOT NULL, -- ID of the ship
+        port_id INT NOT NULL, -- ID of the port
         eta TIMESTAMP NOT NULL, -- ETA of the waypoint (elapsed time of arrival)
         etd TIMESTAMP NOT NULL, -- ETD of the waypoint (elapsed time of departure)
         hex_color TEXT NOT NULL, -- Color to identify waypoint when displaying, in hex color format
