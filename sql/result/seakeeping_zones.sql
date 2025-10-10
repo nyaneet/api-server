fad50ca5c64fab7bf832aef62a694fc6edafc0a3
Klaimmoro <klaimmor228@gmail.com>
2025-10-10 12:59:31 +0300

    fixing

--- sql/result/seakeeping_zones.sql
+++ sql/result/seakeeping_zones.sql
@@ -1,10 +1,13 @@
 DROP TABLE IF EXISTS seakeeping_zones CASCADE;
 
+DROP TYPE IF EXISTS zone_type CASCADE;
+CREATE TYPE zone_type AS ENUM ('HighWaves', 'Parametric', 'Broaching', 'Main');
+
 CREATE TABLE IF NOT EXISTS seakeeping_zones (
     id SERIAL PRIMARY KEY,
     ship_id INT NOT NULL,
     angle FLOAT NOT NULL,
     speed FLOAT NOT NULL,
-    zone_id VARCHAR(20) NOT NULL CHECK (zone_id IN ('HighWaves', 'Parametric', 'Broching', 'Main')),
+    zone_id zone_type,
     CONSTRAINT unique_angle_speed_zone UNIQUE (angle, speed, zone_id)
 );
--- sql/result/seakeeping_zones_status.sql
+++ sql/result/seakeeping_zones_status.sql
@@ -1,8 +1,11 @@
 DROP TABLE IF EXISTS seakeeping_zones_status CASCADE;
 
+DROP TYPE IF EXISTS zone_type CASCADE;
+CREATE TYPE zone_type AS ENUM ('HighWaves', 'Parametric', 'Broaching', 'Main');
+
 CREATE TABLE IF NOT EXISTS seakeeping_zones_status (
     id SERIAL PRIMARY KEY,
     ship_id INT NOT NULL,
-    zone_id VARCHAR(20) NOT NULL CHECK (zone_id IN ('HighWaves', 'Parametric', 'Broching', 'Main')),
+    zone_id zone_type,
     zone_status BOOLEAN NOT NULL
 );
