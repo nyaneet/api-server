0a542b27237554d4060d3ffbea6cb4108e109a6c
Klaimmoro <klaimmor228@gmail.com>
2025-10-10 15:49:41 +0300

    New tag for seakeeping

--- sql/result/seakeeping_zones_status.sql
+++ sql/result/seakeeping_zones_status.sql
@@ -1,8 +1,5 @@
 DROP TABLE IF EXISTS seakeeping_zones_status CASCADE;
 
-DROP TYPE IF EXISTS zone_type CASCADE;
-CREATE TYPE zone_type AS ENUM ('HighWaves', 'Parametric', 'Broaching', 'Main');
-
 CREATE TABLE IF NOT EXISTS seakeeping_zones_status (
     id SERIAL PRIMARY KEY,
     ship_id INT NOT NULL,
