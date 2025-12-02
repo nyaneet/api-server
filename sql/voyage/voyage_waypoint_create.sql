358770221492c714ce8dc0e451b6dbde09d8d302
povergo <korolev.petr2003@gmail.com>
2025-12-02 21:59:42 +0300

    DB, waypoint | Removed port_code and port_name

--- sql/voyage/voyage_waypoint_create.sql
+++ sql/voyage/voyage_waypoint_create.sql
@@ -7,8 +7,6 @@ CREATE TABLE
         project_id INT, -- ID of the project
         ship_id INT NOT NULL, -- ID of the ship
         port_id INT, -- ID of the port
-        port_name TEXT NOT NULL, -- Name of the port (or port city)
-        port_code TEXT NOT NULL, -- Code of the port
         eta TIMESTAMP NOT NULL, -- ETA of the waypoint (elapsed time of arrival)
         etd TIMESTAMP NOT NULL, -- ETD of the waypoint (elapsed time of departure)
         hex_color TEXT NOT NULL, -- Color to identify waypoint when displaying, in hex color format
