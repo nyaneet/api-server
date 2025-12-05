115f32e33c74fee26e59aa7c5a65a76b2868c77f
nyaneet <nyaneet@gmail.com>
2025-12-05 21:13:03 +0300

    DB, general cargo | add test entries in shape table

--- sql/shape/shape.sql
+++ sql/shape/shape.sql
@@ -31,3 +31,29 @@ CREATE TABLE IF NOT EXISTS shape_parameter (
     CONSTRAINT shape_parameter_shape_fk FOREIGN KEY (shape_id) REFERENCES shape (id),
     CONSTRAINT shape_parameter_unique UNIQUE(shape_id, name)
 );
+
+-- Test data:
+-- cuboid
+WITH cuboid_id AS (
+    INSERT INTO shape (type) VALUES ('cuboid')
+    RETURNING id
+)
+INSERT INTO shape_parameter (shape_id, name, value) VALUES
+((SELECT id FROM cuboid_id), 'width', 8.0),
+((SELECT id FROM cuboid_id), 'height', 4.0),
+((SELECT id FROM cuboid_id), 'depth', 2.0);
+-- sphere
+WITH sphere_id AS (
+    INSERT INTO shape (type) VALUES ('sphere')
+    RETURNING id
+)
+INSERT INTO shape_parameter (shape_id, name, value) VALUES
+((SELECT id FROM sphere_id), 'radius', 5.0);
+-- cylynder longitudinal
+WITH cylinder_id AS (
+    INSERT INTO shape (type) VALUES ('cylinder_longitudinal')
+    RETURNING id
+)
+INSERT INTO shape_parameter (shape_id, name, value) VALUES
+((SELECT id FROM cylinder_id), 'length', 8.0),
+((SELECT id FROM cylinder_id), 'radius', 4.0);
