300bd59570a4397a9784755e7454886f121278af
nyaneet <nyaneet@gmail.com>
2025-12-10 15:29:58 +0300

    DB, general cargo | add space table

---
+++ sql/space/space.sql
@@ -0,0 +1,21 @@
+--
+DROP TABLE IF EXISTS space CASCADE;
+
+CREATE TABLE IF NOT EXISTS space (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    ship_id INT NOT NULL,
+    project_id INT,
+    space_code TEXT NOT NULL,
+    name_rus TEXT NOT NULL,
+    name_engl TEXT NOT NULL,
+    bound_x1 FLOAT8 NOT NULL,
+    bound_x2 FLOAT8 NOT NULL,
+    bound_y1 FLOAT8 NOT NULL,
+    bound_y2 FLOAT8 NOT NULL,
+    bound_z1 FLOAT8 NOT NULL,
+    bound_z2 FLOAT8 NOT NULL,
+    CONSTRAINT space_pk PRIMARY KEY (id),
+    CONSTRAINT space_bounds_x_check CHECK (bound_x1 <= bound_x2),
+    CONSTRAINT space_bounds_y_check CHECK (bound_y1 <= bound_y2),
+    CONSTRAINT space_bounds_z_check CHECK (bound_z1 <= bound_z2)
+);
