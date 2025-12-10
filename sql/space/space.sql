8091e4b1ad86ce8a5858ef9e963d3b55e3a8c96a
nyaneet <nyaneet@gmail.com>
2025-12-10 15:30:50 +0300

    DB, general cargo | add test entries in space table

--- sql/space/space.sql
+++ sql/space/space.sql
@@ -19,3 +19,57 @@ CREATE TABLE IF NOT EXISTS space (
     CONSTRAINT space_bounds_y_check CHECK (bound_y1 <= bound_y2),
     CONSTRAINT space_bounds_z_check CHECK (bound_z1 <= bound_z2)
 );
+
+-- Sofia test examples
+INSERT INTO space (
+    ship_id,
+    project_id,
+    space_code,
+    name_rus,
+    name_engl,
+    bound_x1,
+    bound_x2,
+    bound_y1,
+    bound_y2,
+    bound_z1,
+    bound_z2
+) VALUES
+(
+    2,
+    NULL,
+    '00-0',
+    'Судно внешнее',
+    'Ship Bounding',
+    -10.0,
+    140.0,
+    -8.0,
+    8.0,
+    0.0,
+    28.0
+),
+(
+    2,
+    NULL,
+    '11R.1',
+    'Жилой блок',
+    'Accommodation',
+    4.2,
+    10.9,
+    -6.6,
+    6.6,
+    12.38,
+    14.95
+),
+(
+    2,
+    NULL,
+    '10Y-1',
+    'Палубная погрузочная зона',
+    'Deck loading zone',
+    -10.0,
+    140.0,
+    -8.0,
+    8.0,
+    28.0,
+    28.0
+);
