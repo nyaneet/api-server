0936f961fe8e35af5089343c5fc93beedd400c33
nyaneet <nyaneet@gmail.com>
2025-12-11 18:18:43 +0300

    Db, shape | change parameters names in test examples

--- sql/shape/shape.sql
+++ sql/shape/shape.sql
@@ -32,28 +32,28 @@ CREATE TABLE IF NOT EXISTS shape_parameter (
     CONSTRAINT shape_parameter_unique UNIQUE(shape_id, name)
 );
 
--- Test data:
+-- Test examples:
 -- cuboid
-WITH cuboid_id AS (
+WITH new_shape_id AS (
     INSERT INTO shape (type) VALUES ('cuboid')
     RETURNING id
 )
 INSERT INTO shape_parameter (shape_id, name, value) VALUES
-((SELECT id FROM cuboid_id), 'width', 8.0),
-((SELECT id FROM cuboid_id), 'height', 4.0),
-((SELECT id FROM cuboid_id), 'depth', 2.0);
+((SELECT id FROM new_shape_id), 'xSize', 8.0),
+((SELECT id FROM new_shape_id), 'ySize', 4.0),
+((SELECT id FROM new_shape_id), 'zSize', 2.0);
 -- sphere
-WITH sphere_id AS (
+WITH new_shape_id AS (
     INSERT INTO shape (type) VALUES ('sphere')
     RETURNING id
 )
 INSERT INTO shape_parameter (shape_id, name, value) VALUES
-((SELECT id FROM sphere_id), 'radius', 5.0);
--- cylynder longitudinal
-WITH cylinder_id AS (
+((SELECT id FROM new_shape_id), 'radius', 5.0);
+-- cylinder longitudinal
+WITH new_shape_id AS (
     INSERT INTO shape (type) VALUES ('cylinder_longitudinal')
     RETURNING id
 )
 INSERT INTO shape_parameter (shape_id, name, value) VALUES
-((SELECT id FROM cylinder_id), 'length', 8.0),
-((SELECT id FROM cylinder_id), 'radius', 4.0);
+((SELECT id FROM new_shape_id), 'xSize', 8.0),
+((SELECT id FROM new_shape_id), 'radius', 4.0);
