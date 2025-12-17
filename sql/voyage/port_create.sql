160153fbd54f99c1b1a6b2563c579cba03004b4d
povergo <korolev.petr2003@gmail.com>
2025-12-17 21:41:15 +0300

    DB, port | Added missing comma

--- sql/voyage/port_create.sql
+++ sql/voyage/port_create.sql
@@ -10,7 +10,7 @@ CREATE TABLE port (
     CONSTRAINT port_pk PRIMARY KEY (id),
     CONSTRAINT port_name_ru_length_check CHECK (char_length(port_name_ru) > 0),
     CONSTRAINT port_name_en_length_check CHECK (char_length(port_name_en) > 0),
-    CONSTRAINT port_code_length_check CHECK (char_length(port_code) BETWEEN 1 AND 5)
+    CONSTRAINT port_code_length_check CHECK (char_length(port_code) BETWEEN 1 AND 5),
     -- CONSTRAINT port_code_unique UNIQUE (port_code),
     CONSTRAINT latitude_range_check CHECK (latitude >= -90 AND latitude <= 90),
     CONSTRAINT longitude_range_check CHECK (longitude >= -180 AND longitude <= 180)
