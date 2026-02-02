0e1ac5b2ba4622cb31177808ec115e4ae76c83e7
pivovarvalera <pivovarvaleri@gmail.com>
2026-02-02 18:34:07 +0300

    DB, route_point_create | fixes

--- sql/voyage/route_point_create.sql
+++ sql/voyage/route_point_create.sql
@@ -5,10 +5,10 @@ CREATE TABLE IF NOT EXISTS route_point (
     pol_id INT NOT NULL, -- ID of the POL (Point Of Loading)
     pod_id INT NOT NULL, -- ID of the POD (Point Of Discharge)
     latitude FLOAT NOT NULL, -- Value of latitude
-    longitude FLOAT NOT NULL, -- Value of latitude
+    longitude FLOAT NOT NULL, -- Value of longitude
     order INT NOT NULL, -- Order of route point
     CONSTRAINT route_point_pk PRIMARY KEY (id),
     CONSTRAINT route_point_pol_fk FOREIGN KEY (pol_id) REFERENCES waypoint (id),
     CONSTRAINT route_point_pod_fk FOREIGN KEY (pod_id) REFERENCES waypoint (id),
-    CONSTRAINT unique_route_order UNIQUE (pol_id, pod_id, point_order)
+    CONSTRAINT unique_route_point_order UNIQUE (pol_id, pod_id, order)
 );
