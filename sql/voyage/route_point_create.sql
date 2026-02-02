b0d02abf821f1b827a24851bf96be39e01a9e11e
pivovarvalera <pivovarvaleri@gmail.com>
2026-02-02 16:06:38 +0300

    DB, route_point_create |  corrected pk, file moved to sql/voyage

--- sql/voyage/route_point_create.sql
+++ sql/voyage/route_point_create.sql
@@ -1,12 +1,13 @@
 DROP TABLE IF EXISTS route_point;
 
 CREATE TABLE IF NOT EXISTS route_point (
-    id SERIAL PRIMARY KEY, -- ID of the route point
+    id INT GENERATED ALWAYS AS IDENTITY, -- ID of the route point
     pol_id INT NOT NULL, -- ID of the POL (Point Of Loading)
     pod_id INT NOT NULL, -- ID of the POD (Point Of Discharge)
     latitude FLOAT NOT NULL, -- Value of latitude
     latitude FLOAT NOT NULL, -- Value of latitude
     point_order INT NOT NULL, -- Order of route point
+    CONSTRAINT route_point_pk PRIMARY KEY (id),
     CONSTRAINT route_point_pol_fk FOREIGN KEY (pol_id) REFERENCES waypoint (id),
     CONSTRAINT route_point_pod_fk FOREIGN KEY (pod_id) REFERENCES waypoint (id),
     CONSTRAINT unique_route_order UNIQUE (pol_id, pod_id, point_order)
