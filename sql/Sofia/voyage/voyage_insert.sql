570b287132eeae76f4d19b38a9a6b585e94f6675
nyaneet <nyaneet@gmail.com>
2026-02-05 19:01:22 +0300

    DB, Voyage | replace initial data for voyage name, code, and description fields in ARK and Sofia to russian equivalents

--- sql/Sofia/voyage/voyage_insert.sql
+++ sql/Sofia/voyage/voyage_insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO voyage
     (ship_id, project_id, name, code, description, density, operational_speed, wetting_timber, icing_type_id, icing_timber_type_id, water_area_id, load_line_id)
 VALUES 
-    (2, NULL, 'Voyage name', 'Voyage code', 'Voyage description', 1.025, 14.7, 10, 1, 1, 1, 1); -- TODO replace with initial data
+    (2, NULL, 'Имя рейса', 'Код рейса', 'Описание рейса', 1.025, 14.7, 10, 1, 1, 1, 1); -- TODO replace with initial data
